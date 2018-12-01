b1f6a19176fb6ffd64ab525569f2fdead02a5a9d
Syrus Akbary <me@syrusakbary.com>
2018-12-01 13:19:51 -0800

    Add emscripten support

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 CC=gcc
 
-all:
+all: emscripten
 	$(CC) -Wall -Werror -o main main.c
 	strip main
+
+emscripten:
+	emcc -Wall -Werror -o main.js main.c
--- main.c
+++ main.c
@@ -9,20 +9,25 @@
 #include <signal.h>
 #include "ipaddress.h"
 
-void send_response(FILE *f, char *key, char *value) {
+void send_response(FILE *f, char *key, char *value)
+{
   fprintf(f, "HTTP/1.1 200 OK\r\n");
   fprintf(f, "Content-Type: application/json\r\n");
   fprintf(f, "\r\n");
   fprintf(f, "{\"%s\": \"%s\"}", key, value);
 }
 
-int open_connection(int port) {
+int open_connection(int port)
+{
   int sock;
   struct sockaddr_in addr_in;
 
+  printf("simpleserver::Creating socket\n");
   sock = socket(AF_INET, SOCK_STREAM, 0);
+  printf("simpleserver::Socket created (%d)\n", sock);
 
-  if (sock == -1) {
+  if (sock == -1)
+  {
     printf("Failed to create socket (%d)\n", errno);
     exit(EXIT_FAILURE);
   }
@@ -31,9 +36,14 @@ int open_connection(int port) {
   addr_in.sin_port = htons(port);
   addr_in.sin_addr.s_addr = INADDR_ANY;
 
-  bind(sock, (struct sockaddr *) &addr_in, sizeof(struct sockaddr_in));
+  printf("simpleserver::bind\n");
 
-  if (listen(sock, 8) == -1) {
+  bind(sock, (struct sockaddr *)&addr_in, sizeof(struct sockaddr_in));
+
+  printf("simpleserver::listen\n");
+
+  if (listen(sock, 8) == -1)
+  {
     printf("Failed to get socket to listen (%d)\n", errno);
     exit(EXIT_FAILURE);
   }
@@ -41,13 +51,15 @@ int open_connection(int port) {
   return sock;
 }
 
-void accept_client(int sock, char* key) {
+void accept_client(int sock, char *key)
+{
   struct sockaddr_in client_addr;
   socklen_t clientaddr_len;
   FILE *f;
 
   int client_sock = accept(sock, (struct sockaddr *)&client_addr, &clientaddr_len);
-  if (client_sock == -1) {
+  if (client_sock == -1)
+  {
     printf("Failed to accept connection (%d)\n", errno);
     exit(EXIT_FAILURE);
   }
@@ -66,22 +78,27 @@ void accept_client(int sock, char* key) {
  * This HTTP server can only send HTTP responses.
  * It requires header files such as ipaddress.h to read the headers.
  */
-void send_only(int port, char* key) {
+void send_only(int port, char *key)
+{
   int sock = open_connection(port);
 
-  while (1) {
+  while (1)
+  {
     accept_client(sock, key);
   }
   close(sock);
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    printf("%s [port-number]\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
+int main(int argc, char **argv)
+{
+  // if (argc != 2)
+  // {
+  //   printf("%s [port-number]\n", argv[0]);
+  //   exit(EXIT_FAILURE);
+  // }
 
-  int port = atoi(argv[1]); 
+  // int port = atoi(argv[1]);
+  int port = 3090;
   send_only(port, "address");
 
   exit(EXIT_SUCCESS);
